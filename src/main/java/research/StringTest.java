fbd980af3540c073dbd786972685039665afe495
Yevhen_Tkachenko <Yevhen_Tkachenko@epam.com>
2023-09-07 16:26:07 +0300

    update research: String test

---
+++ src/main/java/research/StringTest.java
@@ -0,0 +1,10 @@
+package research;
+
+public class StringTest {
+
+    public static void main(String[] args) {
+
+        String s = String.format("%d(%%) - percentage", 70);
+        System.out.println(s);
+    }
+}
